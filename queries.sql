b1683211ec6c72554e44fbe26b782dc76fff7cb5
Daryl Bonny <darylbonny@Daryls-MacBook-Pro.local>
2023-02-24 18:03:28 -0500

    did it

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
 -- Part 1: Test it with SQL
-
+CREATE TABLE job (
+    id INT,
+    employer VARCHAR(255),
+    name VARCHAR(255),
+    skills VARCHAR(255)
+);
 -- Part 2: Test it with SQL
+Select name
+FROM employer
+WHERE location = "St. Louis City"
 
 -- Part 3: Test it with SQL
+DROP TABLE job
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+INNER JOIN job ON job.skills = skill.id
+WHERE job.id IS NOT NULL
+ORDER BY skill.name ASC
